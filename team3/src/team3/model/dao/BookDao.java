e23ce574599bf89691908a08b27a7c3b81d77a66
504 <504@DESKTOP-8HL92AU>
2023-07-28 17:29:52 +0900

    0728 17:29

--- team3/src/team3/model/dao/BookDao.java
+++ team3/src/team3/model/dao/BookDao.java
@@ -4,5 +4,5 @@ import team3.model.dto.BookDto;
 
 public class BookDao {
 	// 책들의 정보를 저장하는 공간/배열
-	static BookDto[] BookList = new BookDto[100];
+	public static BookDto[] BookList = new BookDto[100];
 }
