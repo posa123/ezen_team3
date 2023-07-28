bbd9c7d8d779003d8c7d7d7b86061a1c08822d1d
504 <504@DESKTOP-8HL92AU>
2023-07-28 17:22:32 +0900

    0728 5:22

--- team3/src/team3/model/dao/BookDao.java
+++ team3/src/team3/model/dao/BookDao.java
@@ -1,5 +1,8 @@
 package team3.model.dao;
 
-public class BookDao {
+import team3.model.dto.BookDto;
 
+public class BookDao {
+	// 책들의 정보를 저장하는 공간/배열
+	static BookDto[] BookList = new BookDto[100];
 }
--- team3/src/team3/model/dto/BookDto.java
+++ team3/src/team3/model/dto/BookDto.java
@@ -1,5 +1,53 @@
 package team3.model.dto;
 
 public class BookDto {
-
-}
+	// 1. 필드
+	private String 책이름;
+	private String 작가;
+	private String 출판사;
+	private boolean 대출여부;
+	// 2. 생성자
+	public BookDto(String 책이름, String 작가, String 출판사 ) {
+		super();
+		this.책이름 = 책이름;
+		this.작가 = 작가;
+		this.출판사 = 출판사;
+		this.대출여부 = true;
+	}
+	public BookDto() {
+		// TODO Auto-generated constructor stub
+	}
+		// 3. 메소드
+	
+	public String get책이름() {
+		
+		return 책이름;
+	}
+	
+	public void set책(String 책이름) {
+		
+		this.책이름 = 책이름;
+	}
+	
+	public String get작가() {
+		return 작가;
+	}
+	
+	public void set작가(String 작가) {
+		this.작가 = 작가;
+	}
+	
+	public String get출판사() {
+		return 출판사;
+	}
+	
+	public void set출판사(String 출판사) {
+		this.출판사 = 출판사;
+	}
+	
+	@Override
+	public String toString() {
+		return "BookDto [책이름=" + 책이름+ ", 작가=" + 작가 + ", 출판사=" + 출판사 + "]";
+	}
+		
+	}
