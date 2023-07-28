c88ea9a735547206891ab14f9544cce3d3c8c785
504 <504@DESKTOP-8HL92AU>
2023-07-28 17:25:11 +0900

    0728 17:25

--- team3/src/team3/model/dto/BookDto.java
+++ team3/src/team3/model/dto/BookDto.java
@@ -18,36 +18,36 @@ public class BookDto {
 		// TODO Auto-generated constructor stub
 	}
 		// 3. 메소드
-	
 	public String get책이름() {
-		
 		return 책이름;
 	}
-	
-	public void set책(String 책이름) {
-		
+	public void set책이름(String 책이름) {
 		this.책이름 = 책이름;
 	}
-	
 	public String get작가() {
 		return 작가;
 	}
-	
 	public void set작가(String 작가) {
 		this.작가 = 작가;
 	}
-	
 	public String get출판사() {
 		return 출판사;
 	}
-	
 	public void set출판사(String 출판사) {
 		this.출판사 = 출판사;
 	}
-	
+	public boolean is대출여부() {
+		return 대출여부;
+	}
+	public void set대출여부(boolean 대출여부) {
+		this.대출여부 = 대출여부;
+	}
 	@Override
 	public String toString() {
-		return "BookDto [책이름=" + 책이름+ ", 작가=" + 작가 + ", 출판사=" + 출판사 + "]";
+		return "BookDto [책이름=" + 책이름 + ", 작가=" + 작가 + ", 출판사=" + 출판사 + ", 대출여부=" + 대출여부 + "]";
 	}
+	
+
 		
 	}
+
