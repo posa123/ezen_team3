7487e81a8713a74d39280a85f4a8b99a7e10afb5
504 <504@DESKTOP-TTN284H>
2023-08-11 16:56:20 +0900

    0811 , 16:56 정희락

--- team3/src/project_csb/controller/CompanySysController.java
+++ team3/src/project_csb/controller/CompanySysController.java
@@ -1,5 +1,6 @@
 package project_csb.controller;
 
+import project_csb.model.dao.CompanySysDao;
 
 //회사 시스템 컨트롤러
 public class CompanySysController {
@@ -7,4 +8,19 @@ public class CompanySysController {
 		private static CompanySysController controller = new CompanySysController();
 		private CompanySysController() {}
 		public static CompanySysController getInstance() {return controller;}
+		
+		// 1. 배송 등록하기 메소드
+		public boolean boxRegistration() {
+			CompanySysDao.getInstance().boxRegistration();
+			
+			return false;
+		}
+		
+		// 2. 배송 관리하기 메소드 
+		public boolean boxManagement() {
+			
+			
+			return false;
+		}
+		
 }
