b4be560a3e09307601e8aa977b15020b0172e454
504 <504@DESKTOP-8HL92AU>
2023-08-14 15:57:25 +0900

    Merge branch 'main' of https://github.com/posa123/ezen_team3 into 한승우

--- team3/src/project_csb/controller/RiderPhoneController.java
+++ team3/src/project_csb/controller/RiderPhoneController.java
@@ -1,5 +1,10 @@
 package project_csb.controller;
 
+import java.util.ArrayList;
+
+import project_csb.model.dao.RiderPhoneDao;
+import project_csb.model.dto.RiderPhoneDto;
+
 // 기사 핸드폰 컨트롤러
 public class RiderPhoneController {
 	// 싱글톤
@@ -8,6 +13,10 @@ public class RiderPhoneController {
 	public static RiderPhoneController getInstance() {return controller;}
 	
 	
-	public 
+	
+	// 자신에게 할당된 배달건 확인
+	public ArrayList<RiderPhoneDto> boxRegistList(){
+		return RiderPhoneDao.getInstance().boxRegistList( PhoneMainController.getInstance().getPhoneSession());
+	}
 	
 }
--- team3/src/project_csb/model/dao/RiderPhoneDao.java
+++ team3/src/project_csb/model/dao/RiderPhoneDao.java
@@ -1,11 +1,39 @@
 package project_csb.model.dao;
 
+import java.util.ArrayList;
 
+import project_csb.database.ConnectJdbc;
+import project_csb.model.dto.RiderPhoneDto;
 
 //기사 핸드폰 dao
-public class RiderPhoneDao {
+public class RiderPhoneDao extends ConnectJdbc{
 	// 싱글톤
 	private static RiderPhoneDao dao = new RiderPhoneDao();
 	private RiderPhoneDao() {}
 	public static RiderPhoneDao getInstance() {return dao;}	
+	
+	
+	// 자신에게 할당된 배달건 확인
+	public ArrayList<RiderPhoneDto> boxRegistList( String phoneNumber ){
+		ArrayList<RiderPhoneDto> list = new  ArrayList<>();
+		int barticle = 0;
+		try {
+			String sql = "select barticle from couriertale where barphone = ?";
+			ps = conn.prepareStatement(sql);
+			rs = ps.executeQuery();
+			if(rs.next()) {
+				barticle = rs.getInt(1);
+			}
+		} catch (Exception e) {System.out.println(e);}
+		try {
+			String sql = "select * from Delivery_status where barticle = " +barticle;
+			ps = conn.prepareStatement(sql);
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				RiderPhoneDto dto = new RiderPhoneDto(rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getInt(4), rs.getString(5), rs.getBoolean(6));
+				list.add(dto);
+			}	
+		} catch (Exception e) {System.out.println(e);}
+		return list;
+	}
 }
