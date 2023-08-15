f4f38e79331916e5edd19a75007890b565cabfd7
naa15 <naa15@DESKTOP-HEQMIGV>
2023-08-15 11:33:37 +0900

    11:33

--- team3/src/project_csb/controller/UserPhoneController.java
+++ team3/src/project_csb/controller/UserPhoneController.java
@@ -3,25 +3,31 @@ package project_csb.controller;
 
 
 import java.util.ArrayList;
-
 import project_csb.model.dao.UserPhoneDao;
-import project_csb.model.dto.UserPhoneDto;
 
 // 유저 핸드폰 컨트롤러
 public class UserPhoneController {
 	// 싱글톤
-	private static UserPhoneController controller = new UserPhoneController();
+	private static UserPhoneController userPhoneController = new UserPhoneController();
 	private UserPhoneController() {}
-	public static UserPhoneController getInstance() {return controller;}
+	public static UserPhoneController getInstance() {return userPhoneController;}
 	
-	// 송장번호 확인하는메소드 
-	public ArrayList<Integer> invoiceNumber(String phoneSession) {
+	/*
+	 *  송장번호 확인하는메소드 
+	 */
+	public ArrayList<Integer> invoiceNumber() {
 		
 		
 		// MainPhoneController.getInstance().getPhoneSession() 에 값이들어오는걸 dao에게 전달 
-		return UserPhoneDao.getInstance().invoiceNumber(MainPhoneController.getInstance().getPhoneSession());
-		
+		return UserPhoneDao.getInstance().invoiceNumber(MainPhoneController.getInstance().getPhoneSession());	
 	}
 	
-	
+	/*
+	 * 배송상태 확인하는 메소드
+	 */
+	public int checkDeliveryStatus( int invoiceNumber) {
+		
+		
+		return -1;
+	}
 }
--- team3/src/project_csb/model/dao/UserPhoneDao.java
+++ team3/src/project_csb/model/dao/UserPhoneDao.java
@@ -1,11 +1,8 @@
 package project_csb.model.dao;
 
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-
 import project_csb.database.ConnectJdbc;
-import project_csb.model.dto.UserPhoneDto;
 
 //유저 핸드폰 dao
 public class UserPhoneDao extends ConnectJdbc {
@@ -17,20 +14,21 @@ public class UserPhoneDao extends ConnectJdbc {
 	public ArrayList<Integer> invoiceNumber(String phoneSession){
 		// 배열선언하는이유는 유저가 전화번호를 입력했을때 배송상태/송장번호가 여러개 일수있기때문에 그갯수만큼 넣는다 
 			// 배열선언을 해주지않으면 여러개의 배송정보를 부를수가없다.
-		ArrayList<Integer> invoicelist = new ArrayList<>(); 
+		ArrayList<Integer> invoiceList = new ArrayList<>(); 
 		try {
 			// SQL작성한다
 				// 송장번호를 보기위해 , 배송상태를 확인하기위해 유저핸드폰번호를 입력한다.
 			String sql = "select InvoiceNumber from Delivery_status where Customer_phone_numbe = ? ";
 			ps = conn.prepareStatement(sql);
+			ps.setString(1, phoneSession);
 			rs = ps.executeQuery();
 			while(rs.next()) {	
 				// 배열안에 넣는다
-				invoicelist.add(rs.getInt(1));
-				
+				invoiceList.add(rs.getInt(1));			
 			}// w e	
 		}catch (Exception e) {System.out.println(e);}
-		return invoicelist;
+		//배열 반환
+		return invoiceList;
 	}
 	
 }
