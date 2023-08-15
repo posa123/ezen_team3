aaeb0bc4cfc39e7e533e570e4824129eafb0e940
kpopgoodlee <kpopgoodlee@localhost>
2023-08-15 15:53:25 +0900

    이성호 커밋 0815/15:53

--- team3/src/project_csb/view/RiderView.java
+++ team3/src/project_csb/view/RiderView.java
@@ -12,5 +12,10 @@ public class RiderView implements MainInterface{
 	@Override
 	public void OutPutFront() {
 		System.out.println("\n\n기사 view 실행");
+		//택배사 입력하기
+		System.out.println("택배사 입력하세요 : ");
+		//기사 인증하기
+		//보관함과 비밀번호 설정하기
+		//고객(사용자)에게 보관함 번호와 비밀번호 전달하
 	}
 }
